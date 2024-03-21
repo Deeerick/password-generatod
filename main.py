e22c8bc35fa59f15bf1cc9aab071975f7bd31500
Deeerick <dspsoares@outlook.com>
2024-03-21 14:27:43 -0300

    First Commit

---
+++ main.py
@@ -0,0 +1,23 @@
+print('=' * 10 + '\033[91m 🚨 Password Generator 🚨 \033[0m' + '=' * 10)
+
+import random
+import string
+
+def gerar_senha(tamanho):
+    caracteres = string.ascii_letters + string.digits + string.digits + "!@#?" * 2
+    senha = ''.join(random.choice(caracteres) for _ in range(tamanho))
+    return senha
+
+while True:
+    tamanho_senha = int(input("Qual o tamanho da senha? "))
+    senha_gerada = gerar_senha(tamanho_senha)
+    print("Sua senha é: ", senha_gerada)
+
+    while True:
+        opcao = input("Deseja refazer a senha? (S/N): ")
+        if opcao.lower() == 's':
+            break
+        elif opcao.lower() == 'n':
+            exit()
+        else:
+            print("Opção inválida. Por favor, responda com 'S' ou 'N'.")
